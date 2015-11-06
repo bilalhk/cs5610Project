56d219afca0300d933bf24f3ac4473ae6a36473b
Bilal Khan <khan.b@husky.neu.edu>
2015-11-05 22:14:22 -0800

    Created Start New Game page.

--- public/config.js
+++ public/config.js
@@ -9,6 +9,12 @@
 			.when("/profile", {
 				templateUrl: "profile/profile.view.html"
 			})
+			.when("/new_game", {
+				templateUrl: "new_game/new_game.view.html"
+			})
+			.when("/leaderboard", {
+				templateUrl: "leaderboard/leaderboard.view.html"
+			})
 			.otherwise({
 				redirectTo: "/login"
 			});
